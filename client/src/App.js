e7121e91762b5bf4852bd768cf77ae2a52f37cb9
durganp1 <durganp1@gmail.com>
2020-10-21 15:50:35 -0500

    added apollo server set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+// APOLLO PROVIDER WILL PROVIDE DATA TO AL OF THE OTHER COMPONENTS
+import {ApolloProvider} from '@apollo/react-hooks';
+// APOLLO CLIENT WILL GET THAT DATA
+import ApolloClient from 'apollo-boost';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// ESTABLISH CONNECTION TO THE BACK-END SERVERS ENDPOINT
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+});
+
 function App() {
   return (
+    <ApolloProvider client = {client}>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
@@ -14,6 +24,7 @@ function App() {
       </div>
       <Footer />
     </div>
+    </ApolloProvider>
   );
 }
 
