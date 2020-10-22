b27463ff809d0c9da684474ad358691faaa5f7d5
durganp1 <durganp1@gmail.com>
2020-10-22 12:09:51 -0500

    added new thought functionality

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,6 +6,7 @@ import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
 import {Redirect, useParams} from 'react-router-dom';
 import {ADD_FRIEND} from '../utils/mutations';
+import ThoughtForm from '../components/ThoughtForm';
 
 
 
@@ -72,6 +73,7 @@ const Profile = () => {
               />
           </div>
       </div>
+        <div className = 'mb-3'>{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
